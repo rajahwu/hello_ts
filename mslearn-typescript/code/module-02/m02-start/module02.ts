03301110a32da08544f7ac328756961ba9c10aa8
rajahwu <vmradford@gmail.com>
2022-12-17 07:24:30 -0500

    complete module 02

--- mslearn-typescript/code/module-02/m02-start/module02.ts
+++ mslearn-typescript/code/module-02/m02-start/module02.ts
@@ -28,13 +28,13 @@ if (ukCitizen) {
    it to have strongly typed variables. Then, address any errors you find so that the result 
    returned to a is 12. */
 
-let x;
-let y;
-let a;
+let x: number;
+let y: number;
+let a: number;
 
-x = 'five';
+x = 5;
 y = 7;
-a = x + y;
+a = x + y
 
 console.log(a);
 
@@ -43,41 +43,45 @@ console.log(a);
    the constants "Fall", "Winter", "Spring", and "Summer". Then, update the function so 
    you can pass in the season by referencing an item in the enum, for example 
    Season.Fall, instead of the literal string "Fall". */
+enum Season {
+    Fall,
+    Winter,
+    Spring,
+    Summer
+}
 
-function whichMonths(season) {
-
+function whichMonths(season: Season) {
     let monthsInSeason: string;
-
+    
     switch (season) {
-        case "Fall":
+        case Season.Fall:
             monthsInSeason = "September to November";
             break;
-        case "Winter":
+        case Season.Winter:
             monthsInSeason = "December to February";
             break;
-        case "Spring":
+        case Season.Spring:
             monthsInSeason = "March to May";
             break;
-        case "Summer":
+        case Season.Summer:
             monthsInSeason = "June to August";
     }
 
     return monthsInSeason;
 }
 
-console.log(whichMonths("Fall"));
+console.log(whichMonths(Season["Fall"]));
 
 /* EXERCISE 4
    TODO: Declare the array as the type to match the type of the items in the array. */
 
-   let randomNumbers;
-   let nextNumber;
-   
-   for (let i = 0; i < 10; i++) {
-       nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-       randomNumbers.push(nextNumber);
-   }
-   
-   console.log(randomNumbers);
+let randomNumbers: number[] = [];
+let nextNumber: number;
 
-   
+for (let i = 0; i < 10; i++) {
+    nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    randomNumbers.push(nextNumber);
+}
+
+console.log(randomNumbers);
+
